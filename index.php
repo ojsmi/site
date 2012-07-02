54ca321feb977ed5aaf95be091e09b77ef20a89e
Oliver Smith <contact@ollyjsmith.com>
2012-07-02 16:45:58 +0100

    content update

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<div id="content">
 		<hr><!-- provides a visual break if styles are disabled (hidden in stylesheet) -->
 		<header id="about" class="about">
-			<p class="large-text"><span class="red">Oliver Smith</span> works with code and emergent technologies. He is currently investigating networked, spatialised objects and environments and his work continues to explore the principles and possibilities of self organisation.</p>
+			<p class="large-text"><span class="red">Oliver Smith</span> is an artist working with code and emergent technologies.</p>
 			<br/>
 			<ul class="small-text">
 				<li class="red">Twitter: <a href="http://twitter.com/ollyjsmith">@ollyjsmith</a></li>
